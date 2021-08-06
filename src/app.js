e0a7014b0f1f35cb244cdbb9895120a32c5a0470
florscaglione <florencia.scaglione@hotmail.com>
2021-08-06 00:28:43 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function generadorDominios(element) {};
+
+let domainNames = [];
+let pronombres = ["the", "our"];
+let adjetivos = ["great", "big"];
+let sustantivos = ["jogger", "racoon"];
+let extensiones = ["com", "es"];
+
+for (let a = 0; a < pronombres.length; a++) {
+  let pronombre = pronombres[a];
+
+  for (let b = 0; b < adjetivos.length; b++) {
+    let adjetivo = adjetivos[b];
+
+    for (let c = 0; c < sustantivos.length; c++) {
+      let sustantivo = sustantivos[c];
+
+      for (let d = 0; c < extensiones.length; d++) {
+        let extension = extensiones[d];
+
+        domainNames.push(pronombre + adjetivo + sustantivo + extension);
+      }
+    }
+  }
+}
+
+console.log(domainNames); //function appendDomainNames(element) {
+//  element.innerHTML = "";
+//  console.log(domainNames.join(""));
+//}
