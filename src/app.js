25c965175a33b69a2592acfe9e91f2fdf310982f
florscaglione <florencia.scaglione@hotmail.com>
2021-10-02 19:45:30 +0000

    error corregido

--- src/app.js
+++ src/app.js
@@ -5,30 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function generadorDominios(element) {};
+window.onload = function generadorDominios(element) {
+  let domainNames = [];
+  let pronombres = ["the", "our"];
+  let adjetivos = ["great", "big"];
+  let sustantivos = ["jogger", "racoon"];
+  let extensiones = [".com", ".es"];
 
-let domainNames = [];
-let pronombres = ["the", "our"];
-let adjetivos = ["great", "big"];
-let sustantivos = ["jogger", "racoon"];
-let extensiones = [".com", ".es"];
+  for (let a = 0; a < pronombres.length; a++) {
+    let pronombre = pronombres[a];
 
-for (let a = 0; a < pronombres.length; a++) {
-  let pronombre = pronombres[a];
+    for (let b = 0; b < adjetivos.length; b++) {
+      let adjetivo = adjetivos[b];
 
-  for (let b = 0; b < adjetivos.length; b++) {
-    let adjetivo = adjetivos[b];
+      for (let c = 0; c < sustantivos.length; c++) {
+        let sustantivo = sustantivos[c];
 
-    for (let c = 0; c < sustantivos.length; c++) {
-      let sustantivo = sustantivos[c];
+        for (let d = 0; d < extensiones.length; d++) {
+          let extension = extensiones[d];
 
-      for (let d = 0; d < extensiones.length; d++) {
-        let extension = extensiones[d];
-
-        domainNames.push(pronombre + adjetivo + sustantivo + extension);
+          domainNames.push(pronombre + adjetivo + sustantivo + extension);
+        }
       }
     }
   }
-}
 
-console.log(domainNames);
+  console.log(domainNames);
+};
